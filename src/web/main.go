ba21fc1585501b61e62badd258eb84c403fb15b9
hph <admin@hepeihua.com>
2019-10-07 17:30:21 +0800

    web

---
+++ src/web/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	//simpleWeb()
+	//customRouteHandler()
+	fileServer()
+}
+
+/* 简单输出 */
+func simpleWeb() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(*r)
+		fmt.Fprintf(w, "hello world")
+	})
+	//不设置handler就用默认的DefaultServeMux
+	http.ListenAndServe(":8081", nil)
+}
+
+/* 自定义简易路由 */
+type myRouteHandler struct {
+}
+
+func (*myRouteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "hello world")
+		return
+	}
+	if r.URL.Path == "/fuck" {
+		fmt.Fprintf(w, "fuck you")
+		return
+	}
+	if r.URL.Path == "/post" && r.Method == "POST" {
+		r.ParseForm() //解析url传递的参数，对于POST则解析响应包的主体（request body）
+
+		//打印form表单
+		fmt.Println(r.Form)
+		//打印form某个值
+		fmt.Println(r.Form["name"])
+		//遍历表单
+		for k, v := range r.Form {
+			fmt.Fprintln(w, "key:", k)
+			fmt.Fprintln(w, "val:", strings.Join(v, ","))
+		}
+
+		return
+	}
+
+	http.NotFound(w, r)
+	return
+}
+
+func customRouteHandler() {
+	handler := &myRouteHandler{}
+	http.ListenAndServe(":8081", handler)
+}
+
+/* 文件上传 */
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	if r.Method == "POST" {
+		buf := make([]byte, 1<<10)
+		hash := md5.New()
+		//处理文件上传我们需要调用r.ParseMultipartForm，
+		//里面的参数表示maxMemory，调用ParseMultipartForm之后，上传的文件存储在maxMemory大小的内存里面，
+		//如果文件大小超过了maxMemory，那么剩下的部分将存储在系统的临时文件中
+		//10mb, 2^10=1024
+		r.ParseMultipartForm(10 << 20)
+		fmt.Println("SetParseMultipartForm: 10mb")
+
+		//我们可以通过r.FormFile获取上面的文件句柄
+		file, handler, err := r.FormFile("file")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		wfile, err := os.OpenFile("./upload/"+handler.Filename, os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer wfile.Close()
+		totalLen := handler.Size
+		var totalRead int64
+		for totalRead < totalLen {
+			len, err := file.Read(buf)
+			if err == nil {
+				totalRead += int64(len)
+				hash.Write(buf[:len])
+				wfile.Write(buf[:len])
+			}
+		}
+
+		fmt.Fprintln(w, "md5:", hex.EncodeToString(hash.Sum(nil)))
+		fmt.Fprintln(w, "File:", wfile.Name())
+	}
+
+}
+
+func fileServer() {
+	os.Mkdir("./upload", 0777)
+	http.HandleFunc("/upload", upload)
+	http.ListenAndServe(":8081", nil)
+}
