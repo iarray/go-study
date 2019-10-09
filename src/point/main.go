a2fc94a7689081219a51276dab3b0871e606b6b4
iarray <admin@hepeihua.com>
2019-10-09 15:03:00 +0800

    value copy

--- src/point/main.go
+++ src/point/main.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
- 
+
+type MyStruct struct{
+	a int 
+	b string
+	c float32
+}
+
 func main(){
 	i := 5
 	p := &i
@@ -19,10 +25,24 @@ func main(){
 	p3 := &p
 
 	fmt.Println(p3, *p3, **p3)
-	fmt.Printf("%T", p3)
+	fmt.Printf("%T\n", p3)
 	
 
-	var p4 *int = nil
-	*p4 = 0
-	fmt.Println(p4, *p4)
+	//error
+	// var p4 *int = nil
+	// *p4 = 0
+	// fmt.Println(p4, *p4)
+
+	str1 := MyStruct{1,"2",3.0}
+	//引用复制还是值复制 ?
+	str2 := str1
+	str2.a = 4
+	str2.b = "5"
+	str2.c = 6.0
+
+	//结果是值复制, str1和str2指向的内存是不同的
+	//将str1赋给str2会两步走: 1.为str2分配空间, 2.把str1的值复制到str2
+	//同样的, 返回结构体也会造成值复制, 
+	fmt.Println(str1)
+	fmt.Println(str2)
 }
